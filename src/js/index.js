00ddb7fcb3e8bd0602345c31c1ace18b390f3731
mordenius <mordenius@gmail.com>
2019-11-14 13:49:45 +0200

    feature(index) message split counter

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
-module.exports = function messageSplitCounter(s, k) {
-	return 1;
+const _SPACE_CHAR = " ";
+const _SPACE_CHAR_LENGTH = 1;
+
+function messageSplitCounter(inputString, maxLength) {
+	let chunkLength = 0;
+	let wordLength = 0;
+	let messageCount = 1;
+
+	for (const char of inputString) {
+		if (char === _SPACE_CHAR) {
+			if (maxLength < wordLength) {
+				return -1;
+			} else if (maxLength < chunkLength + wordLength) {
+				chunkLength += _SPACE_CHAR_LENGTH + wordLength;
+			} else {
+				messageCount += 1;
+				chunkLength = wordLength;
+			}
+		} else {
+			wordLength++;
+		}
+	}
+
+	return messageCount;
 }
+
+module.exports = messageSplitCounter;
